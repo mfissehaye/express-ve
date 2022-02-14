02c4e54b9b5c693eac0ca743771caf87ca2822fc
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2022-02-14 11:44:44 +0300

    fixed issue with config not taking up index.js file inside config directory

--- providers/config-provider.js
+++ providers/config-provider.js
@@ -3,18 +3,26 @@ const pathResolver = require("path");
 const _camelCase = require('lodash/camelCase')
 
 module.exports = async (c) => {
-  const configFiles = await readdirp.promise(pathResolver.resolve('./', 'config'))
-      .then((files) => files.map(f => f.path))
+    const configFiles = await readdirp.promise(pathResolver.resolve('./', 'config'))
+        .then((files) => files.map(f => f.path))
 
-  const configs = {}
-  configFiles.forEach(config => {
-    const key = _camelCase(config.replace(/\.js$/, ''))
-    configs[key] = require(pathResolver.resolve('./config/', config))
-  })
-  c.service('config', () => {
-    return {
-      ...configs,
-      ...process.env
-    }
-  });
+    const configs = {}
+    configFiles.forEach(config => {
+        const key = _camelCase(config.replace(/\.js$/, ''))
+        if (key !== 'index') {
+            configs[key] = require(pathResolver.resolve('./config/', config))
+        } else {
+            const topConfigs = require(pathResolver.resolve('./config'))
+            for (const configKey in topConfigs) {
+                configs[configKey] = topConfigs[configKey]
+            }
+        }
+    })
+    console.log(configs)
+    c.service('config', () => {
+        return {
+            ...configs,
+            ...process.env
+        }
+    });
 };
